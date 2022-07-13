0e7dbc54424cf43907c39355de2cefbd50e33e30
pyris <pyris@gmail.com>
2022-07-13 22:25:11 +0200

    adding all categories to API

--- app/main.py
+++ app/main.py
@@ -9,14 +9,37 @@ ctx.load_cert_chain('app/certs/fullchain.pem','app/certs/privkey.pem')
 app=Flask(__name__)
 CORS(app)
 
-#we are importing our function from the colors.py file
+
+# flask API to predict tweets/text via trained DL/ML model
+
 
 @app.route("/",methods=['GET','POST'])
 def index():
+    '''
+    basic GET API to store annotation + tweet text passed via URL
+    '''
     if request.method=='GET':
-    #getting the url argument       
+        # TEXT
         text = request.args.get('text')
+        # RON user selection + DL prediction
+        ron_selection = request.args.get('ronSelection')
         prediction = predictor.predict(text)
+        prediction['user_prediction'] = ron_selection
+        # RELIGIONS
+        religions = request.args.get('religions')
+        if religions:
+            religions = religions.split('+')
+            religions = [religion for religion in religions if religion != ""]
+        prediction['religions'] = religions
+        # SOCIAL FIELDS
+        social_fields = request.args.get('social_fields')
+        if social_fields:
+            social_fields = social_fields.split('+')
+            social_fields = [field for field in social_fields if field != ""]
+        prediction['social_fields'] = social_fields
+        # SENTIMENT
+        prediction['sentiment'] = request.args.get('sentiment')
+
         # saving texts and predictions
         with open('app/prediction_archive/texts_received.txt', "a", encoding="utf-8") as f:
             f.write(jsonify(prediction).get_data(as_text=True))
